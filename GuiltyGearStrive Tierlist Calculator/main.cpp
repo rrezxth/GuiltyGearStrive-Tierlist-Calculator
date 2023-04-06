90ce67e2659f0d471fc73b83717df0cc5056a206
rrezxth <rienz.edu@icloud.com>
2023-04-06 12:32:00 -0400

    Initial Commit

---
+++ GuiltyGearStrive Tierlist Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GuiltyGearStrive Tierlist Calculator
+//
+//  Created by Rienz Estella on 2023-04-06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
